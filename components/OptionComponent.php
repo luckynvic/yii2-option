a704e2a50485144ff18e8439f47721a7cbd44a9d
Lucky Vic <luckynvic@gmail.com>
2016-08-21 06:49:58 +0800

    add trace info

--- components/OptionComponent.php
+++ components/OptionComponent.php
@@ -13,12 +13,16 @@ class OptionComponent extends Component
 {
     public function get($id, $index=null, $default=null)
     {
-        if(($content = Yii::$app->cache->get(OptionModel::CACHE_PREFIX.$id))===false) {
+        $cache_key = OptionModel::CACHE_PREFIX.$id;
+        if(($content = Yii::$app->cache->get($cache_key))===false) {
             if(($model = OptionModel::findOne($id))===null)
               return $default;
             $content = Json::decode($model->content);
-            Yii::$app->cache->set(OptionModel::CACHE_PREFIX.$id, $content);
-        }
+            Yii::$app->cache->set($cache_key, $content);
+            Yii::trace('Load option from database: '.$id, __METHOD__);
+        } else
+            Yii::trace('Load option from cache: '.$id, __METHOD__);
+
         if($index!==null && is_array($content))
             return array_key_exists($index, $content)?$content[$index]:$default;
         else
